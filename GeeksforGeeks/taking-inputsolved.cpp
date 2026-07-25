112a449d331ef7c1c656f7acba59e2bb053b1f83
Daksh Chaudhary <dakshchoudhary877@gmail.com>
2026-07-25 21:42:58 +0530

    Add Taking InputSolved solution (gfg)

---
+++ GeeksforGeeks/taking-inputsolved.cpp
@@ -0,0 +1,19 @@
+import math
+
+s=input()
+n=int(input())
+f=float(input())
+ff=math.floor(f)
+
+
+
+# s to store string
+# n to store integer
+# f to store float
+# ff  # To Store floor of float variable f
+
+# code here
+
+print(s)
+print(n)
+print(ff)
